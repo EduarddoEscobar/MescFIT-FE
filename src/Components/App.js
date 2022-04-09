6a7b1e3bb7e8e779b56b5d92d013a4973a462bb2
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-04-08 22:02:43 -0400

    Added more stuff to the header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,22 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import Home from './Home/Home';
 import './App.css';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <h2>MescPT</h2>
+        <nav>
+          <Link to="/about">About Me</Link>
+          <Link to="/one-to-onecoaching">1:1 Coaching</Link>
+          <Link to="/testimonials">Testimonials</Link>
+          <Link to="/workout-guides">Workout Guides</Link>
+          <Link to="/pricing">Pricing</Link>
+        </nav>
+        <div className="nav-contact">
+          <a href="https://www.instagram.com/escobarmarkk/" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
+          <Link to="/email-form">Contact</Link>
+        </div>
       </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   );
 }
 
