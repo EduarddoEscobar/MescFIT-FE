4a52334befef242ab15e355ecb4ad32d891f1fdf
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-04-09 17:20:35 -0400

    Made some changes to the header and added some fonts

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,23 +7,25 @@ import './App.css';
 const App = (props) => {
   return (
     <Router>
-      <header>
-        <h2>MescPT</h2>
+        <header>
+        <div className="logo">
+            <Link data-testid="header-link" to="/">MescPT</Link>
+        </div>
         <nav>
-          <Link to="/about">About Me</Link>
-          <Link to="/one-to-onecoaching">1:1 Coaching</Link>
-          <Link to="/testimonials">Testimonials</Link>
-          <Link to="/workout-guides">Workout Guides</Link>
-          <Link to="/pricing">Pricing</Link>
+            <Link data-testid="header-link" to="/about">About Me</Link>
+            <Link data-testid="header-link" to="/one-to-onecoaching">1:1 Coaching</Link>
+            <Link data-testid="header-link" to="/testimonials">Testimonials</Link>
+            <Link data-testid="header-link" to="/workout-catalog">Workout Catalog</Link>
+            <Link data-testid="header-link" to="/pricing">Pricing</Link>
         </nav>
         <div className="nav-contact">
-          <a href="https://www.instagram.com/escobarmarkk/" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
-          <Link to="/email-form">Contact</Link>
+            <a href="https://www.instagram.com/escobarmarkk/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+            <Link className="contact-button" data-testid="header-link" to="/email-form">Contact</Link>
         </div>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-      </Routes>
+        </header>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+        </Routes>
     </Router>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Home = (props) => {
     return(
-        <>
+        <div className="background-cover gym-background homepage">
             <h1>Home Page</h1>
-        </>
+        </div>
     )
 }
 export default Home;
