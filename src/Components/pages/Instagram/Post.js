17fb38776ef6e43ac289e36a7ff8bb2846e2f2ee
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-07-13 04:10:00 -0400

    Got the instagram posts working

---
+++ src/Components/pages/Instagram/Post.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVideo, faCamera } from '@fortawesome/free-solid-svg-icons';
+
+const Post = props => {
+  const {
+    media_type,
+    thumbnail_url,
+    permalink: post_url,
+    caption,
+    children,
+  } = props;
+
+  const getImage = () => {
+    if (media_type !== 'CAROUSEL_ALBUM') return thumbnail_url;
+    return children['data'][0]['media_type'] === 'VIDEO'
+      ? children['data'][0]['thumbnail_url']
+      : children['data'][0]['media_url'];
+  };
+
+  return (
+    <div className="instagram-post-container">
+      <FontAwesomeIcon icon={media_type === 'VIDEO' ? faVideo : faCamera} />
+      <a href={post_url} target="_blank" rel="noopener">
+        <img src={getImage()} alt={caption} />
+      </a>
+    </div>
+  );
+};
+
+export default Post;
