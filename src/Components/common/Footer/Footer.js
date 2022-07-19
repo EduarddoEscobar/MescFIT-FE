f0c36d5bd5b61b9aa388f127f743257182be6dd3
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-07-19 19:48:12 -0400

    Finished with the footer mobile styling

--- src/Components/common/Footer/Footer.js
+++ src/Components/common/Footer/Footer.js
@@ -13,37 +13,45 @@ const Footer = props => {
     <footer>
       {/*Top Half of Footer*/}
       <div className="footer-div footer-links">
-        <div className="social-links footer-content">
-          <IconLink
-            href="https://www.instagram.com/escobarmarkk/"
-            icon={faInstagram}
-          />
-          <IconLink
-            href="https://www.tiktok.com/@escobarmarkk"
-            icon={faTiktok}
-          />
-          <IconLink
-            href="https://youtube.com/channel/UCnVQa9DSntayyDwU5K0-Ivw"
-            icon={faYoutube}
-          />
-        </div>
-        <div className="footer-content main-links">
-          <Link to="/">Home</Link>
-          <Link to="/about">About Me</Link>
-          <Link to="/coaching">Coaching</Link>
-          <Link to="/testimonials">Testimonials</Link>
-          <Link to="/exercise-library">Exercise Library</Link>
-          <Link to="/faq">Pricing / FAQs</Link>
-        </div>
-        <div className="footer-content center-text footer-contact">
-          <a href="mailto:escobarmark@gmail.com" className="underline italics">
-            Email Me
-          </a>
-          <p className="mobile-only"> - </p>
-          <Link to="">Business & Coaching Inquiries</Link>
-        </div>
-        <div className="footer-content center-text copyright">
-          <h2>©2022 by MescFIT</h2>
+        <div className="footer-container">
+          <div className="social-links footer-content">
+            <p className="mobile-only">Follow Me!</p>
+            <div className="icons">
+              <IconLink
+                href="https://www.instagram.com/escobarmarkk/"
+                icon={faInstagram}
+              />
+              <IconLink
+                href="https://www.tiktok.com/@escobarmarkk"
+                icon={faTiktok}
+              />
+              <IconLink
+                href="https://youtube.com/channel/UCnVQa9DSntayyDwU5K0-Ivw"
+                icon={faYoutube}
+              />
+            </div>
+          </div>
+          <div className="footer-content main-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About Me</Link>
+            <Link to="/coaching">Coaching</Link>
+            <Link to="/testimonials">Testimonials</Link>
+            <Link to="/exercise-library">Exercise Library</Link>
+            <Link to="/faq">Pricing / FAQs</Link>
+          </div>
+          <div className="footer-content center-text footer-contact">
+            <a
+              href="mailto:escobarmark@gmail.com"
+              className="underline italics"
+            >
+              Email Me
+            </a>
+            <p className="mobile-only"> - </p>
+            <Link to="#">Business & Coaching Inquiries</Link>
+          </div>
+          <div className="footer-content center-text copyright">
+            <h2>©2022 by MescFIT</h2>
+          </div>
         </div>
       </div>
 
